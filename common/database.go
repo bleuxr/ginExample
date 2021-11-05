bd648d64ef916c4d33dc09bcf10b7ea0811724c7
spencer <spencer@localhost.localdomain>
2021-11-05 19:10:58 +0800

    从文件中读取配置

--- common/database.go
+++ common/database.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"ginExample/model"
 
+	"github.com/spf13/viper"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -11,8 +12,26 @@ import (
 var DB *gorm.DB
 
 func InitDB() *gorm.DB {
-	dsn := "root:MyNewPass4!@tcp(127.0.0.1:3306)/mydb?charset=utf8mb4&parseTime=True&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	// driverName := viper.GetString("datasource.driverName")
+	host := viper.GetString("datasource.host")
+	port := viper.GetString("datasource.port")
+	database := viper.GetString("datasource.database")
+	username := viper.GetString("datasource.username")
+	password := viper.GetString("datasource.password")
+	charset := viper.GetString("datasource.charset")
+	loc := viper.GetString("datasource.loc")
+	args := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=%s&parseTime=True&loc=%s",
+		username,
+		password,
+		host,
+		port,
+		database,
+		charset,
+		loc)
+	fmt.Printf(args)
+	// dsn := "root:MyNewPass4!@tcp(127.0.0.1:3306)/mydb?charset=utf8mb4&parseTime=True&loc=Local"
+	// db, err := gorm.Open(driverName.Open(args), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(args), &gorm.Config{})
 	if err != nil {
 		fmt.Printf("fail to connect database! err:" + err.Error())
 	}
--- main.go
+++ main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"ginExample/common"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	InitConfig()
 	common.InitDB()
 	// common.InitDB()
 	// db.Close() is removed from v2
@@ -14,6 +17,21 @@ func main() {
 
 	r := gin.Default()
 	r = CollectRouter(r)
+	port := viper.GetString("server.port")
+	if port != "" {
+		panic(r.Run(":" + port))
+	}
+	panic(r.Run())
+	// r.Run() // listen and serve on 0.0.0.0:8080
+}
 
-	r.Run() // listen and serve on 0.0.0.0:8080
+func InitConfig() {
+	workDir, _ := os.Getwd()
+	viper.SetConfigName("application")
+	viper.SetConfigType("yml")
+	viper.AddConfigPath(workDir + "/config")
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
 }
